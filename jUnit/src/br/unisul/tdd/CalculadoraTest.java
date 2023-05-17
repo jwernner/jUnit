4b452294d9159f083bf271bd85d6a55559fd6754
jwernner <jorge.werner@gmail.com>
2023-05-17 12:04:56 -0300

    Validando teste da soma

--- jUnit/src/br/unisul/tdd/CalculadoraTest.java
+++ jUnit/src/br/unisul/tdd/CalculadoraTest.java
@@ -10,9 +10,10 @@ public class CalculadoraTest {
 	public void deveriaSomarDoisNumerosPositivos() {
 		
 		Calculadora calc = new Calculadora();
+
 		int soma = calc.somar(3, 7);
 		
 		Assert.assertEquals(10, soma);
 
-}
+	}
 }
--- jUnit/src/br/unisul/tdd/CalculadoraTeste.java
+++ jUnit/src/br/unisul/tdd/CalculadoraTeste.java
@@ -11,14 +11,16 @@ public class CalculadoraTeste {
 		
 		soma = calc.somar(3, 0);
 		System.out.println(soma);
-		
+
 		soma = calc.somar(0, 0);
 		System.out.println(soma);
-		
+
 		soma = calc.somar(3, -2);
 		System.out.println(soma);
-	
-
+		
 	}
 	
 }
+
+
+
